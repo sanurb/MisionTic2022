2b4c580bd2098a9dd1f9ff16539c870d919659bd
SantiagoUrbano <david.urbano@uao.edu.co>
2022-11-27 11:09:56 -0500

    Servicios listo

--- ciclo4_javascript/inmuebles-frontend/src/app/servicios/inmueble.service.ts
+++ ciclo4_javascript/inmuebles-frontend/src/app/servicios/inmueble.service.ts
@@ -1,9 +1,55 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-
+import { Observable } from 'rxjs';
+import { InmuebleModel } from '../modelos/Inmueble.model';
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class InmuebleService {
+  url: string = 'http://localhost:3000/inmueble';
+  constructor(private http: HttpClient) {}
 
-  constructor() { }
+  /**
+   * Lista los registros de la base de datos
+   * @returns arreglo con los objetos de los registros
+   */
+
+  ListarRegistros(): Observable<InmuebleModel[]> {
+    return this.http.get<InmuebleModel[]>(this.url);
+  }
+
+  /**
+   * Busca un registro por Id
+   * @param id id del registro a buscar
+   * @returns el registro del id ingresado
+   */
+
+  BuscaRegistroPorId(id: string): Observable<InmuebleModel> {
+    return this.http.get<InmuebleModel>(this.url + '/' + id);
+  }
+
+  /**
+   * Almacena un nuevo registro
+   * @param nombre propiedad del registro
+   * @returns registro creado
+   */
+
+  GuardarRegistro(registro: InmuebleModel): Observable<InmuebleModel> {
+    return this.http.post<InmuebleModel>(this.url, {
+      propietario: registro.propietario,
+      direccion: registro.direccion,
+      telefono: registro.telefono,
+      tipoInmuebleId: registro.tipoInmuebleId,
+    });
+  }
+
+  /**
+   * ELimina un registro de la base de datos
+   * @param id id del registro a eliminar
+   * @returns NA
+   */
+
+  EliminarRegistro(id: string): Observable<any> {
+    return this.http.delete<any>(this.url + '/' + id);
+  }
 }
--- ciclo4_javascript/inmuebles-frontend/src/app/servicios/tipo-inmueble.service.ts
+++ ciclo4_javascript/inmuebles-frontend/src/app/servicios/tipo-inmueble.service.ts
@@ -19,4 +19,45 @@ export class TipoInmuebleService {
   ListarRegistros(): Observable<TipoInmuebleModel[]> {
     return this.http.get<TipoInmuebleModel[]>(this.url);
   }
+
+  /**
+   * Busca un registro por Id
+   * @param id id del registro a buscar
+   * @returns el registro del id ingresado
+   */
+
+  BuscaRegistroPorId(id: string): Observable<TipoInmuebleModel> {
+    return this.http.get<TipoInmuebleModel>(this.url + '/' + id);
+  }
+
+  /**
+   *  Almacena un nuevo registro
+   * @param nombre propiedad del registro
+   * @returns registro creado
+   */
+
+  GuardarRegistro(nombre: string): Observable<TipoInmuebleModel> {
+    return this.http.post<TipoInmuebleModel>(this.url, { nombre: nombre });
+  }
+
+  /**
+   *  Almacena un nuevo registro
+   * @param id id del registro
+   * @param nombre nuevo nombre
+   * @returns Observable vacío
+   */
+
+  EditarRegistro(id: string, nombre: string): Observable<any> {
+    return this.http.put<any>(this.url + '/' + id, { nombre: nombre });
+  }
+
+  /**
+   *  Almacena un nuevo registro
+   * @param id id del registro a eliminar
+   * @returns NA
+   */
+
+  EliminarRegistro(id: string): Observable<any> {
+    return this.http.delete<any>(this.url + '/' + id);
+  }
 }
